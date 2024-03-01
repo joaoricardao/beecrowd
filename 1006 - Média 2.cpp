3e96b6bc8dede6b23e0a49203ef673df856143f0
João Ricardo <32642843+joaoricardao@users.noreply.github.com>
2024-03-01 16:21:17 -0300

    Create 1006 - Média 2.cpp

---
+++ 1006 - Média 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+ 
+int main() {
+    double A, B, C, MEDIA;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    
+    MEDIA = ((A*2)+(B*3)+(C*5))/10;
+    
+    cout << "MEDIA = " << fixed << setprecision(1) << MEDIA << "\n";
+
+ 
+    return 0;
+}
