b1ddde73dfa27dc13867f7413b23de93032f6448
João Ricardo <32642843+joaoricardao@users.noreply.github.com>
2024-03-02 14:45:52 -0300

    Create 1007 - Diferença.cpp

---
+++ 1007 - Diferença.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int A, B, C, D, DIFERENCA;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> D;
+    DIFERENCA = ((A*B)-(C*D));
+    cout << "DIFERENCA = " << DIFERENCA << "\n";
+     
+    return 0;
+}
